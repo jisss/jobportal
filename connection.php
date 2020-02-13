880ff2c1bff8fa98b38c4ac498e2b9a6fd7804bf
jisss <58838871+jisss@users.noreply.github.com>
2020-02-13 13:15:15 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -5,7 +5,7 @@ var $con,$res;
 
 function getconnect()
 {
-$this->con=mysqli_connect("localhost","root","");
+$this->con=mysqli_connect("localhost","root","123");
 mysqli_select_db($this->con,"jobportal");
 }
 
